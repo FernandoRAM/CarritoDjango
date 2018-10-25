504c365abb3eebc3dc05992aa6eb8e5b53b00d02
Fernando R <frincon20@alumnos.uaq.mx>
2018-10-25 16:42:23 -0500

    Views nuevo

--- apps/carrito/views.py
+++ apps/carrito/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
 from django.conf import settings
-from apps.administrador import models as ad
-from apps.carrito import models
+from appPrueba import models
+from Apps.administrador import models as ad
+from Apps.carrito import models
+
+# Create your views here.
 
 def inicio(request):
 	hola = cargar()
@@ -36,23 +38,35 @@ def log(request):
 	return render(request, 'log.html')
 
 def categorias(request, idC):
-	if idC != '':
-		if idC == '0':
-			diccionario={}
-			array = []
-			categorias = ad.Categoria.objects.all();
-			for i in categorias:
-				array.append([int(idC),i.id, i.Nombre])
-			diccionario["cate"] = array
-			return render(request, 'categorias.html', diccionario)
-		elif idC != '0':
-			diccionario={}
-			array = []
-			categorias = ad.Producto.objects.filter(idCategoria_id=int(idC));
+	
+	if idC is not None and idC == '' or idC =='0':
+		print(idC)
+		idC = 0
+		diccionario={}
+		array = []
+		categorias = ad.Categoria.objects.all();
+		for i in categorias:
+			array.append([idC,i.id, i.Nombre])
+		diccionario["cate"] = array
+		return render(request, 'categorias.html', diccionario)
+		#return HttpResponse(1)
+	else:
+		diccionario={}
+		array = []
+		print (idC)
+		try:
+			categorias = ad.Producto.objects.filter(idCategoria_id=idC);
+			#print(categorias)
 			for x in categorias:
-				array.append([int(idC),x.id,x.Nombre,x.Precio,x.Descripcion,x.idCategoria_id,x.idSucursal_id,x.Ruta])
+				array.append([idC,x.id,x.Nombre,x.Precio,x.Descripcion,x.idCategoria_id,x.idSucursal_id,x.Ruta])
 			diccionario["cate"] = array
 			return render(request, 'categorias.html', diccionario)
+		except Exception as e:
+			diccionario = {}
+
+			return render(request, 'categorias.html')
+		
+
 
 def registro(request):
 	nombre = request.POST.get('nom') 
